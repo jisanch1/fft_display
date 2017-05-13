8f129775bd801b1a0cc38c33f150684cb3f783c7
Javier Sánchez <jisanch1@gmail.com>
2017-05-13 17:49:37 -0300

    implementacion con clases

--- display.py
+++ display.py
@@ -3,79 +3,66 @@ from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 import sys
 
-'''
 class Screen:
 
 	def __init__(self, x, y, fx, fy):
-		print "Initializing... \t",
-		self.app = QtGui.QApplication([])
+		print "Initializing..."
 		self.view = pg.GraphicsView()
-		self.l = pg.GraphicsLayout(border=(100,100,100))
-		self.view.setCentralItem(l)
+		self.layout = pg.GraphicsLayout(border=(100,100,100))
+		self.view.setCentralItem(self.layout)
 		self.view.show()
 		self.view.setWindowTitle('FFT display')
 		self.view.resize(800,600)
-		self.p1 = l.addPlot(title="Time", col=1)
+		self.p1 = self.layout.addPlot(title="Time", col=1)
 		self.p1.setRange(yRange=[-y, y])
 		self.p1.setRange(xRange=[0, x])
-		self.l.nextRow()
-		self.p2 = l.addPlot(title="FFT", col=1)
+		self.layout.nextRow()
+		self.p2 = self.layout.addPlot(title="FFT", col=1)
 		self.p2.setRange(yRange=[0, fy])
 		self.p2.setRange(xRange=[0, fx])
+		self.curve1 = self.p1.plot()
+		self.curve2 = self.p2.plot(stepMode=True, fillLevel=0)
+		print "Creating buffers..."
+		self.pos = 0
+		self.len = x
+		self.time = np.arange(x)
+		self.data = np.zeros(x, float)
+		self.freq = np.arange(fx + 1)
+		self.dataf = np.zeros(fx, float)
 		print "initialization complete."
-'''
-y = 2
-x = 355
-fx = 101
-fy = 2
 
-print "Initializing... \t",
+
+	def plot(self):
+		self.curve1.setData(self.time, self.data, pen=None, symbol='o', symbolPen=None, symbolSize=3, symbolBrush=('b'))
+		
+
+	def plot_fft(self): 
+		self.curve2.setData(self.freq, self.dataf)
+
+	def next_point(self, point):
+		self.data[self.pos] = point
+		self.pos += 1
+		self.pos %= self.len
+
+		
+
+## Testbench
 app = QtGui.QApplication([])
-view = pg.GraphicsView()
-l = pg.GraphicsLayout(border=(100,100,100))
-view.setCentralItem(l)
-view.show()
-view.setWindowTitle('FFT display')
-view.resize(800,600)
-p1 = l.addPlot(title="Time", col=1)
-p1.setRange(yRange=[-y, y])
-p1.setRange(xRange=[0, x])
-l.nextRow()
-p2 = l.addPlot(title="FFT", col=1)
-p2.setRange(yRange=[0, fy])
-p2.setRange(xRange=[0, fx])
-print "initialization complete."
 
-curve1 = p1.plot()
-
-
+length = 355
+screen = Screen(length, 2, 100, 2)
 time = 0
-data = np.zeros((x,2),float)
-timeRed = np.arange(101)
-dataRed = np.zeros(100)
 
-curve2 = p2.plot(stepMode=True, fillLevel=0)
-
-def plot():
+def run():
+	global screen
 	global time
-	global data
-	global dataRed
 	volt = np.sin(np.pi*time/100)
-	tup = (time % x, volt)
-	data[time % x] = np.array(tup)
-	#curve1.setData(data, pen=None, symbol='o', symbolPen=None, symbolSize=3, symbolBrush=('b'))
-	curve1.setData(data, pen=None, symbol='o', symbolPen=None, symbolSize=3, symbolBrush=('b'))
+	screen.next_point(volt)
+	screen.plot()
 	time += 1
-	dataRed[(10 * time + 5) % 100] = volt
-	curve2.setData(timeRed, dataRed) 
 
-def update():
-	plot()
 
 timer = pg.QtCore.QTimer()
-timer.timeout.connect(update)
+timer.timeout.connect(run)
 timer.start(10) #milliseconds
-
 app.instance().exec_()
-
-## Start Qt event loop unless running in interactive mode.
