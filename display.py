a551d98b372dd8be3369a04e7ccf2054589f86ef
Javier Sánchez <jisanch1@gmail.com>
2017-05-13 15:20:42 -0300

    display de la fft

--- display.py
+++ display.py
@@ -3,6 +3,31 @@ from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 import sys
 
+'''
+class Screen:
+
+	def __init__(self, x, y, fx, fy):
+		print "Initializing... \t",
+		self.app = QtGui.QApplication([])
+		self.view = pg.GraphicsView()
+		self.l = pg.GraphicsLayout(border=(100,100,100))
+		self.view.setCentralItem(l)
+		self.view.show()
+		self.view.setWindowTitle('FFT display')
+		self.view.resize(800,600)
+		self.p1 = l.addPlot(title="Time", col=1)
+		self.p1.setRange(yRange=[-y, y])
+		self.p1.setRange(xRange=[0, x])
+		self.l.nextRow()
+		self.p2 = l.addPlot(title="FFT", col=1)
+		self.p2.setRange(yRange=[0, fy])
+		self.p2.setRange(xRange=[0, fx])
+		print "initialization complete."
+'''
+y = 2
+x = 355
+fx = 101
+fy = 2
 
 print "Initializing... \t",
 app = QtGui.QApplication([])
@@ -12,34 +37,37 @@ view.setCentralItem(l)
 view.show()
 view.setWindowTitle('FFT display')
 view.resize(800,600)
-print "initialization complete."
-
-nsamples = 555 #Number of lines for the data
-
 p1 = l.addPlot(title="Time", col=1)
-p1.setRange(yRange=[-1, 1])
-p1.setRange(xRange=[0, nsamples])
+p1.setRange(yRange=[-y, y])
+p1.setRange(xRange=[0, x])
 l.nextRow()
 p2 = l.addPlot(title="FFT", col=1)
-p2.setRange(yRange=[0, 10])
-p2.setRange(xRange=[0, 120])
+p2.setRange(yRange=[0, fy])
+p2.setRange(xRange=[0, fx])
+print "initialization complete."
 
 curve1 = p1.plot()
-curve2 = p2.plot()
+
 
 time = 0
-data = np.zeros((nsamples,2),float)
+data = np.zeros((x,2),float)
+timeRed = np.arange(101)
+dataRed = np.zeros(100)
+
+curve2 = p2.plot(stepMode=True, fillLevel=0)
 
 def plot():
 	global time
 	global data
+	global dataRed
 	volt = np.sin(np.pi*time/100)
-	tup = (time % nsamples, volt)
-	data[time % nsamples] = np.array(tup)
-
-	curve1.setData(data, pen=None, symbol='o', symbolPen=None, symbolSize=2, symbolBrush=('b'))
+	tup = (time % x, volt)
+	data[time % x] = np.array(tup)
+	#curve1.setData(data, pen=None, symbol='o', symbolPen=None, symbolSize=3, symbolBrush=('b'))
+	curve1.setData(data, pen=None, symbol='o', symbolPen=None, symbolSize=3, symbolBrush=('b'))
 	time += 1
-    #curve2.setData(dataRed, pen=None, symbol='o', symbolPen=None, symbolSize=4, symbolBrush=('r')) 
+	dataRed[(10 * time + 5) % 100] = volt
+	curve2.setData(timeRed, dataRed) 
 
 def update():
 	plot()
