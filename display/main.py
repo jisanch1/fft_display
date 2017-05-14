b5348b44adfa9d8c24b9a5bc305c0f475bca36ba
Javier Sánchez <jisanch1@gmail.com>
2017-05-13 23:41:59 -0400

    se agrega comunicacion serial

--- display/main.py
+++ display/main.py
@@ -6,20 +6,17 @@ from serial import Serial
 from display import Screen
 
 length = 343
+time = 0
 
 def run():
 	global screen
 	global stream
-
-	volt = np.sin(np.pi*time/34)
-	screen.next_point(volt)
+	global time
+	if (stream.in_waiting > 10):
+		val = stream.readline()
+		screen.next_point(float(val))
 	if (time % 1 == 0):
 		screen.plot()
-	if (time % 1 == 0):
-		screen.update_fft((time % 10)*10, volt)
-	if (time % 10 == 0):
-		screen.plot_fft()
-	time += 1
 
 
 
@@ -28,17 +25,18 @@ app = QtGui.QApplication([])
 timer = pg.QtCore.QTimer()
 
 port = sys.argv[1]
-baud = sys.argv[2]
+if (sys.argv[2] != None):
+	baud = sys.argv[2]		
+else:
+	baud = 9600		
 
 screen = Screen(length, 2, 100, 2)
 stream = Serial(port, baud)
-
-time = 0
-
-
 
 timer.timeout.connect(run)
 timer.start(10) #milliseconds
 print "Initialization complete."
 app.instance().exec_()
+
+stream.close()
 
